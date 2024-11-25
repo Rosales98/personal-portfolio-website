946e48e8a9b14d5c489e1b60ad294b024714d09d
Claudio <claudio.cruzrosales@gmail.com>
2024-11-25 02:50:15 -0500

    social icons

--- Resources/scripts.js
+++ Resources/scripts.js
@@ -15,4 +15,10 @@ topButton.addEventListener("click", function topFunction() {
     document.documentElement.scrollTop = 0; 
 } );
 
-//
+//current year
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+let currentYearElement = document.getElementById("currentYear");
+currentYearElement.textContent = currentYear;
